574681dffd790b2012d35d1e1985468f3e69f4a9
Lin <forrest.lin@sap.com>
2015-04-30 11:21:59 +0800

    init

--- TestEmbedTableView/ViewController.m
+++ TestEmbedTableView/ViewController.m
@@ -17,6 +17,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    mainTableVIew = [UITableView new];
+    mainTableVIew.frame = self.view.frame;
+    mainTableVIew.delegate = self;
+    mainTableVIew.dataSource = self;
+    [self.view addSubview:mainTableVIew];
+    
+    [mainTableVIew registerClass:[UITableViewCell class] forCellReuseIdentifier:@"mycell"];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 6;
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"mycell" forIndexPath:indexPath];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"mycell"];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"index %ld",indexPath.row];
+    
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
