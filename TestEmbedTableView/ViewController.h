6320f0c063cd12d155c49fc0b6d3be3f93ffd67a
Lin <forrest.lin@sap.com>
2015-04-30 11:12:05 +0800

    Initial Commit

---
+++ TestEmbedTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestEmbedTableView
+//
+//  Created by Lin, Forrest on 4/30/15.
+//  Copyright (c) 2015 Lin, Forrest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestEmbedTableView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestEmbedTableView
+//
+//  Created by Lin, Forrest on 4/30/15.
+//  Copyright (c) 2015 Lin, Forrest. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
